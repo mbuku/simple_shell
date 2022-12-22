d25a59cc38e7822f2e7f58ef2324d4c0bb41034c
Joys mbuku <84926001+mbuku@users.noreply.github.com>
2022-12-22 01:15:09 +0100

    _atoi.c file

--- _atoi.c
+++ _atoi.c
@@ -6,13 +6,9 @@
  *
  * Return: 1 if interactive mode, 0 otherwise
  */
-
 int interactive(info_t *info)
-
 {
-
-return (isatty(STDIN_FILENO) && info->readfd <= 2);
-
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
 
 /**
@@ -21,19 +17,12 @@ return (isatty(STDIN_FILENO) && info->readfd <= 2);
  * @delim: the delimeter string
  * Return: 1 if true, 0 if false
  */
-
 int is_delim(char c, char *delim)
-
 {
-
-while (*delim)
-
-if (*delim++ == c)
-
-return (1);
-
-return (0);
-
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
 
 /**
@@ -43,17 +32,11 @@ return (0);
  */
 
 int _isalpha(int c)
-
 {
-
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-return (1);
-
-else
-
-return (0);
-
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
 
 /**
@@ -63,49 +46,29 @@ return (0);
  */
 
 int _atoi(char *s)
-
 {
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
 
-int i, sign = 1, flag = 0, output;
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
 
-unsigned int result = 0;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
 
-for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
 
-{
-
-if (s[i] == '-')
-
-sign *= -1;
-
-if (s[i] >= '0' && s[i] <= '9')
-
-{
-
-flag = 1;
-
-result *= 10;
-
-result += (s[i] - '0');
-
+	return (output);
 }
-
-else if (flag == 1)
-
-flag = 2;
-
-}
-
-if (sign == -1)
-
-output = -result;
-
-else
-
-output = result;
-
-return (output);
-
-}
-
-
